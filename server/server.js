6e0eabd0a2c22a0d00dca3b6f9dea62a9402b0f6
Ankit Chauhan <ankitchauhan@gmail.com>
2026-02-01 17:53:10 +0530

    Connect MongoDB Database

--- server/server.js
+++ server/server.js
@@ -1,15 +1,23 @@
 import express from "express";
 import "dotenv/config";
 import cors from "cors";
+import connectDB from "./configs/db.js";
+import userRouter from "./Routes/userRoutes.js";
+import ownerRouter from "./Routes/ownerRoutes.js";
 
 // Initialize Express App
 const app = express()
+
+// Connect Database
+await connectDB()
 
 // Middleware
 app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res)=> res.send("Server is running"))
+app.use('/api/user', userRouter)
+app.use('/api/owner', ownerRouter)
 
 const PORT = process.env.PORT || 3000
 app.listen(PORT, ()=> console.log(`Server running on port ${PORT}`))
